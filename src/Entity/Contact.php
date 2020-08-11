2a91eb59be43d3df37d57d125b7fd8f110d806fb
Richard Trujillo Torres <richard.trujillo.torres@gmail.com>
2020-08-11 03:41:07 +0200

    Add email uniqueness constrain

--- src/Entity/Contact.php
+++ src/Entity/Contact.php
@@ -25,7 +25,7 @@ class Contact
     private $uuid;
 
     /**
-     * @ORM\Column(type="string", length=255)
+     * @ORM\Column(type="string", length=255, unique=true)
      */
     private $email;
 
